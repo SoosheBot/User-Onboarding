b08008972aa48f1d9f274c347ae63b7206485f6b
sooshe <sooshe@sooshebot.com>
2019-09-19 17:23:09 -0500

    working on step 4

--- onboarding-builder/src/App.js
+++ onboarding-builder/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import OnboardForm from './components/OnboardForm';
 
+
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
